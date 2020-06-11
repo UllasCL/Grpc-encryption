1cb25381e4866d850a8f4885f3a24dbd766072e7
UllasCL <ullascl.1si15cs127@gmsil.com>
2020-06-11 16:01:15 +0530

    initial commit

---
+++ src/main/java/com/ullas/grpcEncryption/GrpcEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.ullas.grpcEncryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcEncryptionApplication.class, args);
+	}
+
+}
