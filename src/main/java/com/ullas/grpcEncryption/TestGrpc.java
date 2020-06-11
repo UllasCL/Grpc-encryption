79d4ac6d3537fc644c65bee69f1042407fae35ae
UllasCL <ullascl.1si15cs127@gmsil.com>
2020-06-11 17:42:28 +0530

    fixed issue on not able to access generated class

---
+++ src/main/java/com/ullas/grpcEncryption/TestGrpc.java
@@ -0,0 +1,9 @@
+package com.ullas.grpcEncryption;
+
+
+import java.util.logging.Logger;
+
+public class TestGrpc extends TestServiceGrpc.TestServiceImplBase{
+  private static final Logger LOGGER = Logger.getLogger(TestGrpc.class.getName());
+
+}
