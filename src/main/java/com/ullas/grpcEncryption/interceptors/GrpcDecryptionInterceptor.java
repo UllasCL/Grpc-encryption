26532a0f5bd0fb548888a5d8a4d5f49aad500fa6
UllasCL <ullascl.1si15cs127@gmsil.com>
2020-06-16 14:42:15 +0530

    added interceptor

---
+++ src/main/java/com/ullas/grpcEncryption/interceptors/GrpcDecryptionInterceptor.java
@@ -0,0 +1,65 @@
+package com.ullas.grpcEncryption.interceptors;
+
+import io.grpc.Context;
+import io.grpc.Contexts;
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * The type Grpc decryption interceptor.
+ */
+@Component
+@Order(-2147483647)
+public class GrpcDecryptionInterceptor {
+  /**
+   * The constant LOGGER.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(GrpcDecryptionInterceptor.class);
+  /**
+   * The constant NOOP_LISTENER.
+   */
+  private static final ServerCall.Listener NOOP_LISTENER = new ServerCall.Listener() {
+  };
+
+  /**
+   * Instantiates a new Grpc decryption interceptor.
+   */
+  public GrpcDecryptionInterceptor() {
+  }
+
+  /**
+   * Intercept call server call . listener.
+   *
+   * @param <ReqT>  the type parameter
+   * @param <RespT> the type parameter
+   * @param call    the call
+   * @param headers the headers
+   * @param next    the next
+   * @return the server call . listener
+   */
+  public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(final ServerCall<ReqT, RespT> call,
+                                                               final Metadata headers,
+                                                               final ServerCallHandler<ReqT,
+                                                                   RespT> next) {
+    try {
+      LOGGER.info("Inside interceptCall");
+
+      return Contexts.interceptCall(Context.current(), call, headers, next);
+    } catch (StatusRuntimeException var6) {
+      LOGGER.error("Error inside jwt interceptor", var6);
+      call.close(var6.getStatus(), headers);
+      return NOOP_LISTENER;
+    } catch (Exception var7) {
+      LOGGER.error("Error inside jwt interceptor", var7);
+      call.close(Status.INTERNAL.withDescription(var7.getMessage()).withCause(var7), headers);
+      return NOOP_LISTENER;
+    }
+  }
+}
--- src/main/java/com/ullas/grpcEncryption/servers/TestServiceImpl.java
+++ src/main/java/com/ullas/grpcEncryption/servers/TestServiceImpl.java
@@ -5,6 +5,7 @@ import com.ullas.grpcEncryption.EncryptedMessage;
 import com.ullas.grpcEncryption.Request1;
 import com.ullas.grpcEncryption.Response1;
 import com.ullas.grpcEncryption.TestServiceGrpc;
+import com.ullas.grpcEncryption.interceptors.GrpcServerJwtInterceptor;
 import com.ullas.grpcEncryption.utils.AesCryptUtil;
 import com.ullas.grpcEncryption.utils.AesEncryptionUtil;
 import com.ullas.grpcEncryption.utils.EncryptionUtil;
@@ -22,13 +23,13 @@ import org.lognet.springboot.grpc.GRpcService;
 /**
  * The type Test service.
  */
-@GRpcService
+@GRpcService(interceptors = GrpcServerJwtInterceptor.class)
 class TestServiceImpl extends TestServiceGrpc.TestServiceImplBase {
   /**
    * The constant private_key.
    */
   private static String private_key
-      = "";
+      = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCkeqfp3JF/oZKT9cHBPWVIUCAqrXYvbCZmDGCQsgWJ0mO9rViDEjn+4Yqi5GkEcfkLsbFG66hgtlo9He1xiCPF0GsV97YAW8lL+qSEhG9DlNTEqK2LlRyhPqsF8DDQ+dy+7rk3O5A1ZzJg0GlMtqx1Se9p/W3ZqulNSDQkJ1AB+5Fpxmff/88eO8v4gbQII7nVEv28FdQ4S1LsA+8iGiTvTyGQ6t7mUgLlZy/C1W8LJz99CrtU8roZpTvCgtLEdv5AaYZFDNZ3s82ycDeAIBRDvBpfOe9lKeCdOLhUR1R5uRajJWN/CF/7XIh5sWkvX+NqYHL1+QP30e4WVNMFzba/AgMBAAECggEARvO+YYzLmyIo5/jY1zJT8ajW53IijHx76AOo2GJjLzv52MXha9E+AWxr8K7Vepxr0Zaaobxgyy9+p+S0HAidXeT1fOl5spflswfxVlUG5kFas2cMTi1Jx2kQP6SIem1+w0KZ1XC1WcQloEvnULdwl7wIMlGnxIEM2VCvZXUr915MEFg4FPOvGVR/+QPcf6Zi+XA/kTlHYwp3e8M9WrYhuJnaFIs6OsXoDmtg2MI3+Bv9Ddr9Gx3tdZumpsvV2qzbADMIE2PU4QaGygwVDsafZG1IrzAYnMPcZftSDjDy+DEwTAu7gm1tfhXTiyWKSdByO8C0xTmrzxLG4KWvnZ3BoQKBgQDT29Y3RbO2oxnyp6oAL7w8jMadVEhXb+9AR5OK1SA4YT43DlCFjfyF0R33Ex5Y5VQXFVikyGbNiQH0+Hszie2yDQ/mb74IvLN2gtr7SJOUrLGOHse6PZFyxVDPp8mzsVdH7m3ZvSkt/COwsdcieqwsjcIaVmQdhFfJ/YrpvMxeUQKBgQDGv60M9biICDUBybaOR5gjLPDGIvUP76IqHIOBnOSokiBuNvZAn9LZvQIG4/jkqsYd6Va6+QAmz/W9cdYss+WeVwcAh7JII+JcRQuDdKWykgcNMZj5jfOIOP+bJb/EEdPV2S6ZoTEw5YnfUJqPTF7GpKq6npmEnu/VA4fxD5UwDwKBgQCSy4Z4seE+MXEvQ6F1k53YTqroJJgjjbm3TmEbF2XF6wtS1qZQg/tvNWCP7D1g1zYW8IGFXB5vV/l1Jh8nrPB4KLjVsKN6JlYrTawx5S3tHGPC1+0ITNTVM4c0+0u1KLITc0/GSV2ZMVYBfphUma9nB6pw2V9xXFv/56AzKNBqoQKBgQDAG3f2xfCnNF+gKcyWfGd24LKeoh2EWNnDoKoQd4mh0xUb0251isaxBnvWEzd6OAOl1+yob5SQ74SVqFof/YdeZLp07bUK+/yJuYdmejLLh1DflOhLl8UlEAspePSCXcUHuJTNlLxKiiSZQX0UM/qMQZBxMGotgb9Kg8472RcYSwKBgGmOv4Xj7XoP7eTQu0AjJgPv/CGxuKFYxX1/ndA62zwTjXlVleRRCgVjgICA/1YScc/1+cfESGNzV+o0QM+a1drT7yTBQ6lDTCK3kuA9Sq82cnU8UkOQNfSuOA6TCpmeUDePOUiMtzJ/U0+BZUeKMsitanY9qZ9NvOufSOYEns6F";
 
   /**
    * Generate random encryption key string.
